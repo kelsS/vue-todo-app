9cb9b2a35f9304af62cc6202dc9a74af94966210
kelsS <kswanson426@gmail.com>
2018-03-19 19:28:39 -0400

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="todo-container">  
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-for="todo in todos" v-bind:todo="todo"></todo>
+  </section>
+</template>
+
+<script>
+import Todo from './Todo';
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
